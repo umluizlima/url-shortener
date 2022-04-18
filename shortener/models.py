362e7021bd8de2086bebd5ee54983650ecc8c95f
Luiz Lima <umluizlima@gmail.com>
2022-04-17 23:05:38 -0300

    Update URL model to fill hashed_url on save

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,6 @@
+from hashlib import md5
+
+from django.core.validators import URLValidator
 from django.db import models
 
 
@@ -5,3 +8,18 @@ class URL(models.Model):
     long_url = models.URLField(null=False)
     hashed_url = models.SlugField(max_length=200, null=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @staticmethod
+    def _shorten(long_url: str) -> str:
+        return md5(long_url.encode()).hexdigest()[:9]
+
+    @staticmethod
+    def _validate(long_url: str):
+        validate = URLValidator()
+        validate(long_url)
+
+    def save(self, *args, **kwargs):
+        self._validate(self.long_url)
+        if not self.id:
+            self.hashed_url = self._shorten(self.long_url)
+        return super().save(*args, **kwargs)
--- shortener/tests.py
+++ shortener/tests.py
@@ -1,3 +1,18 @@
-# from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.test import TestCase
 
-# Create your tests here.
+from .models import URL
+
+
+class URLModelTests(TestCase):
+    def test_url_is_shortened_on_save(self):
+        long_url = "https://google.com"
+        short_url = URL._shorten(long_url)
+        url = URL(long_url=long_url)
+        url.save()
+        self.assertEqual(url.hashed_url, short_url)
+
+    def test_rejects_invalid_url(self):
+        long_url = "google"
+        url = URL(long_url=long_url)
+        self.assertRaises(ValidationError, url.save)

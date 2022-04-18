b64f5a7d7aa55a78f68f2d23b116ef953ecf95af
Luiz Lima <umluizlima@gmail.com>
2022-04-17 22:51:42 -0300

    Update hashed_url field to SlugField

--- shortener/models.py
+++ shortener/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class URL(models.Model):
     long_url = models.URLField(null=False)
-    hashed_url = models.CharField(max_length=200, null=False, unique=True)
+    hashed_url = models.SlugField(max_length=200, null=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)

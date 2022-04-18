ec6ac3c252cb58be440c5fc83aa1d6f52810fefe
Luiz Lima <umluizlima@gmail.com>
2022-04-17 22:24:11 -0300

    Update database settings to postgres

--- core/settings.py
+++ core/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": "localhost",
+        "NAME": "url-shortener",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
     }
 }
 
